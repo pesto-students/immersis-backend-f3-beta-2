d9d3cf2e1fba3f1267be67f30246ae3d3f770a59
Mehedi Mondal <mehedi.blaze@gmail.com>
2022-02-03 14:28:35 +0530

    add support for csrf

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -15,22 +15,47 @@ describe("Test Authentication Endpoints", () => {
     });
 
     test("/POST Register", async () => {
-        const resp = await request.post("/auth").send({
-            firstName: "test",
-            lastName: "account",
-            email: "test.account@gmail.com",
-            password: "12345678",
-            confirmPassword: "12345678"
+        const preResp = await request.get("/auth/loggedin");
+        const cookie = preResp.headers["set-cookie"];
+        let xsrf;
+        cookie.forEach((elm) => {
+            const token = elm.split("=");
+            if (token[0] === "csrfToken") {
+                xsrf = token.at(1).split(";").at(0);
+            }
         });
+        const resp = await request
+            .post("/auth")
+            .set("cookie", cookie)
+            .set("X-XSRF-TOKEN", xsrf)
+            .send({
+                firstName: "test",
+                lastName: "account",
+                email: "test.account@gmail.com",
+                password: "12345678"
+            });
         expect(resp.statusCode).toBe(200);
         expect(resp.body.success).toBe(true);
     });
 
     test("/POST Login", async () => {
-        const resp = await request.post("/auth/login").send({
-            email: "test.account@gmail.com",
-            password: "12345678"
+        const preResp = await request.get("/auth/loggedin");
+        const cookie = preResp.headers["set-cookie"];
+        let xsrf;
+        cookie.forEach((elm) => {
+            const token = elm.split("=");
+            if (token[0] === "csrfToken") {
+                xsrf = token.at(1).split(";").at(0);
+            }
         });
+        const resp = await request
+            .post("/auth/login")
+            .set("cookie", cookie)
+            .set("X-XSRF-TOKEN", xsrf)
+            .send({
+                email: "test.account@gmail.com",
+                password: "12345678"
+            });
         expect(resp.statusCode).toBe(200);
         expect(resp.body.success).toBe(true);
     });
